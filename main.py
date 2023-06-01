297be00c959ced95f3605f48c5a0d698248fce91
Gabriela M. Fernández <galalou2010@gmail.com>
2023-06-01 19:53:55 +0200

    search functionality

--- main.py
+++ main.py
@@ -56,6 +56,25 @@ def save():
         messagebox.showwarning(title="Try again", message="Please, fill up all the required fields.")
 
 
+def find_pass():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website,message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showerror(title="Error", message=f"We have no passwords for {website}.")
+
+
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -70,11 +89,11 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website: ")
 website_label.grid(row=1,column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
-email_label = Label(text="Email/Username: ")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2,column=0)
 
 email_entry = Entry(width=35)
@@ -89,6 +108,9 @@ password_entry.grid(row=3, column=1)
 
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", width=13, command=find_pass)
+search_button.grid(row=1, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
